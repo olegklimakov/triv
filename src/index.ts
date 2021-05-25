4d3950d05122dd87160330b520a1eef008c6cd64
Oleg <klimakovoleg@yandex.ru>
2021-05-26 00:54:15 +0300

    rename to triv. Added some features

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,72 @@
+#!/usr/bin/env node
+
 const path = process.cwd();
-const fs = require('fs')
 const { exec } = require('child_process')
+const fsPromises = require('fs').promises
 
 console.log(path);
-fs.readFile(path + '/proj-starter.json', 'utf8' , (err: any, data: string) => {
-  if (err) {
-    console.error(err)
-    return
+const install = async () => {
+  const file = await fsPromises.readFile(path + '/proj-starter.json');
+  console.log(file);
+  const parsed = JSON.parse(file)
+  // await execCommand(`npm i ${parsed.repo} --save-dev`)
+  const installInstruction = await require(`${parsed.name}/tech/${parsed.folder}/install.js`);
+  console.log('PATH', installInstruction);
+  console.log('PACKAGE', installInstruction.packages);
+
+  // if (installInstruction.packages?.length) {
+  //   const packagesCommand = `npm i ${installInstruction.packages.join(' ')} --save-dev`;
+  //   await execCommand(packagesCommand);
+  // }
+
+  if (installInstruction.directories?.length) {
+    for (let item of installInstruction.directories) {
+      await handleNestedInstruction(parsed.name, item)
+    }
   }
-  console.log(data)
-  const parsed = JSON.parse(data)
-  console.log(parsed);
-  const child = exec(`npm i ${parsed.repo} --save-dev`);
+  // const file = await fsPromises.readFile('')
+}
+
+const handleNestedInstruction = async (repositoryName: string, instruction: any) => {
+  const address = `${repositoryName}/${instruction.path}/install.js`
+  const installInstruction = await require(address);
+
+  // if (installInstruction?.devPackages) {
+  //   const packagesCommand = `npm i ${installInstruction.devPackages.join(' ')} --save-dev`;
+  //   await execCommand(packagesCommand);
+  // }
+
+  if (installInstruction?.files) {
+    for (let file of installInstruction.files) {
+      const pathToFile = `${repositoryName}/${instruction.path}/${file}`;
+      console.log(pathToFile);
+      await copyFiles(pathToFile, file);
+    }
+  }
+  // console.log(packagesCommand);
+
+  console.log(installInstruction);
+}
+
+const copyFiles = async (pathToFile: string, fileName: string) => {
+  await fsPromises.copyFile(require.resolve(pathToFile), `${path}/${fileName}`);
+}
+
+
+const execCommand = (command: string) => new Promise<void>((resolve, reject) => {
+  const child = exec(command);
   child.stdout.on( 'data', ( data: any ) => {
-    console.log( `stdout: ${ data }` );
+    console.log( `${ data }` );
   } );
 
   child.stderr.on( 'data', ( data: any ) => {
-    console.log( `stderr: ${ data }` );
+    console.error( `${ data }` );
   } );
 
   child.on( 'close', ( code: any ) => {
     console.log( `child process exited with code ${ code }` );
+    resolve();
   } );
 })
+
+install().then()
